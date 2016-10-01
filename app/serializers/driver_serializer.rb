6e0d1e3e0a92d6697d21074eefe5d7013cdff046
Luis Ferrer-Labarca <ferrerluis97@gmail.com>
2016-10-01 15:03:22 -0400

    Customized serializers to present associations

--- app/serializers/driver_serializer.rb
+++ app/serializers/driver_serializer.rb
@@ -3,5 +3,11 @@ class DriverSerializer < ActiveModel::Serializer
   #currently not actually giving data for the location model
   attributes :id, :license_plate, :reviews
   #review has_one location, but is only sending a reference ID
-  #has_many :reviews
+  def reviews
+    object.reviews.as_json only: [:bad_driver, :description], include: {
+      location: {
+        only: [:latitude,:longitude]
+      }
+    }
+  end
 end
--- app/serializers/review_serializer.rb
+++ app/serializers/review_serializer.rb
@@ -1,4 +1,12 @@
 class ReviewSerializer < ActiveModel::Serializer
 	#will return id, description, rating, location, and driver for any given review
-	attributes :id, :description, :bad_driver, :driver, :location
+	attributes :description, :bad_driver
+
+	attribute :location do
+		object.location.as_json only: ['latitude', 'longitude']
+	end
+
+	attribute :driver do
+		object.driver.as_json only: :license_plate
+	end
 end
