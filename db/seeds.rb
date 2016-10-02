a37449e178159e9a63f33dc50150a4a2ec7335ac
Kenneth Hyman <kenneth.hyman@gmail.com>
2016-10-02 00:00:46 -0400

    seeded the database with not so random data and close to tennessee

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,49 @@
 #Just some test data used to populate the database for testing purposes
+class SeedFaker
+	def fake_driver
+		Driver.create(license_plate: fake_plate)
+	end
 
-driver1 = Driver.create(license_plate: "abc 1234")
-driver2 = Driver.create(license_plate: "abc 3456")
-driver3 = Driver.create(license_plate: "abc 6789")
+	def fake_location
+		location = fake_coords
+		Location.create(longitude: location[:longitude], latitude: location[:latitude])
+	end
 
-location1 = Location.create(longitude: 123.456789, latitude: 123.456789)
+	def fake_review(driver, location)
+		Review.create(description: fake_description, bad_driver: asshole_spoof, driver: driver, location: location)
+		#Review.create(description: fake_description, bad_driver: asshole_spoof, location: location, driver: driver)
+	end
+	private
+	def fake_description
+		Faker::Lorem.sentence
+	end
+	def fake_plate
+		length = [6, 7].sample
+		rand(36 ** length).to_s(36)
+	end
+	def fake_coords
+		{latitude: rand(35.950000000...40.000000000), longitude: rand(83.850000000...84.020000000)}
+	end
+	def asshole_spoof
+		[true, true, true, true, true, false].sample
+	end
+end
 
-review1 = Review.create(bad_driver: true, driver: driver1, description: "This guy threw an entire burrito at me while I was driving! I punted his dog off a bridge!", location: location1)
+seeder = SeedFaker.new
+locations = Array.new
+drivers = Array.new
+reviews = Array.new
+20.times do 
+	drivers << seeder.fake_driver
+end
+50.times do 
+	locations << seeder.fake_location
+end
+
+100.times do
+	driver = drivers.sample
+	location = locations.sample
+	reviews << seeder.fake_review(driver, location)
+end
+
+
